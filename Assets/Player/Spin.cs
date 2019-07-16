d9ecba1918d1c766539494b09a7cd3d84b31ae6f
keenanmau <keenanmau@gmail.com>
2019-07-16 11:51:22 -0700

    Initial commit

---
+++ Assets/Player/Spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+
+    public float rotationModifier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * (-1) * rotationModifier);
+    }
+}
---
+++ Assets/Size.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Size : MonoBehaviour
+{
+
+    public float scale = 1;
+    
+    void Update()
+    {
+        increaseScale(1);
+    }  
+
+    public void increaseScale(float value)
+    {
+        scale += value;
+        transform.localScale += new Vector3(0.1F, 0, 0);
+
+    }
+
+    
+}
